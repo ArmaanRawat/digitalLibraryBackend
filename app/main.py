74fd13f55c28dfa48612a97a9a66592fb473f7ad
ArmaanRawat <armaan.rawat0055@gmail.com>
2026-03-31 14:27:34 +0530

    project Setup accroding to Doc

--- app/main.py
+++ app/main.py
@@ -1,14 +1,12 @@
 from fastapi import FastAPI
-from app.routers import users, books, borrow, fines
+from app.routes import auth, books, borrow, fines
 
 app = FastAPI(title="Digital Library Backend")
 
-# Include routers
-app.include_router(users.router, tags=["Users"])
+app.include_router(auth.router, tags=["Auth"])
 app.include_router(books.router, tags=["Books"])
 app.include_router(borrow.router, tags=["Borrow"])
 app.include_router(fines.router, tags=["Fines"])
-
 
 @app.get("/")
 def home():
